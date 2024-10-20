1068ea2a91dee7c3a08a994db0d91abe9bc4f82b
Arham Hussain 🥈 <153752282+ARHAM5432@users.noreply.github.com>
2024-10-20 07:05:48 +0500

    ✔

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-salaam
+// Question 1: Show the current date
+function orginalDate() {
+    let today = new Date();
+    document.write(today);
+}
+
+orginalDate();
+
+// Question 2: Greet the user with their full name
+function greet() {
+    let firstName = prompt("Enter your first name:");
+    let lastName = prompt("Enter your last name:");
+    alert("Hello! " + firstName + " " + lastName);
+}
+
+greet();
+
+// Question 3: Add two numbers and display the result
+function addNum() {
+    let firstNum = +prompt("Enter the first number:");
+    let secondNum = +prompt("Enter the second number:");
+    return xyz = firstNum + secondNum;
+}
+
+addNum();
+let a = xyz;
+document.write(a);
+
+// Question 4: Simple oprulator to perform operations (+, -, *, /)
+function opr() {
+    let firstNum = +prompt("Enter the first number:");
+    let secondNum = +prompt("Enter the second number:");
+    let operation = prompt("Enter the operation you want to do (+, -, *, /):");
+
+    let result;
+
+    switch (operation) {
+        case "+":
+            result = firstNum + secondNum;
+            break;
+        case "-":
+            result = firstNum - secondNum;
+            break;
+        case "*":
+            result = firstNum * secondNum;
+            break;
+        case "/":
+            result = firstNum / secondNum;
+            break;
+        default:
+            result = "Invalid";
+    }
+
+    alert("The result is: " + result);
+}
+
+opr();
